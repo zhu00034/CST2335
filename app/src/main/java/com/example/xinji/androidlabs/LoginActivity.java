18235b4e57e9de66263ef456149849878640928d
xinji <zhu00034@algonquinlive.com>
2018-09-24 23:38:08 -0400

    Lab3

--- app/src/main/java/com/example/xinji/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/xinji/androidlabs/LoginActivity.java
@@ -1,15 +1,80 @@
 package com.example.xinji.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+
+    private static final String ACTIVITY_NAME="LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        final EditText loginEmail = (EditText)findViewById(R.id.loginEmail);
+        final SharedPreferences sharedPref =
+                getSharedPreferences("info", Context.MODE_PRIVATE);
+        String emailName = sharedPref.getString("DefaultEmail", "email@domain.com");
+        loginEmail.setText(emailName);
+
+        Button loginButton =(Button)findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                String myEmail = loginEmail.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                editor.putString("DefaultEmail",myEmail);
+
+                editor.commit();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
 
 
 }
--- app/src/main/java/com/example/xinji/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/xinji/androidlabs/StartActivity.java
@@ -2,12 +2,44 @@ package com.example.xinji.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME="StartActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
 }
