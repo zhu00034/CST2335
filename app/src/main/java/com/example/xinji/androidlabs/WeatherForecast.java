c0bca881f46c9d5d08e0a386c178d753061d5e14
CST2335 <jing.chen@hotmail.ca>
2018-11-03 19:58:42 -0400

    Finished lab6

---
+++ app/src/main/java/com/example/xinji/androidlabs/WeatherForecast.java
@@ -0,0 +1,13 @@
+package com.example.xinji.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherForecast extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+}
